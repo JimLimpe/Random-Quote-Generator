cce816244cbf28c016ac46bb21bb6fd0cdf26464
JimLibe <limperis_dimitris@yahoo.com>
2025-04-25 13:44:50 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import quotes from './components/quotebank.js'
+
+
+function App() {
+  const [quote, setQuote] = useState({}); // state  as an empty object
+
+  const generateQuote = () => {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]; //random quote
+    setQuote(randomQuote);
+  };
+
+  useEffect(() => {
+    generateQuote();
+  }, []); // triggering the first quote on page load (something similar with window.onload on JS)
+  return (
+    <div id="quote-box" className="container-fluid">
+      <h1>Inspiring Quotes</h1>
+      <h2 id="text">{quote.quote}</h2>
+      <h4 id="author">— {quote.writer}</h4>
+      <div className="button-img-wrapper">
+        <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(quote.quote + ' — ' + quote.writer)}`} target="_blank" rel="noreferrer">
+          <img id="xlogo" src="/xlogo.png" alt="Tweet this quote" />
+        </a>
+        <button className="btn btn-primary me-2" id="new-quote" onClick={generateQuote}>
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/quotebank.js
@@ -0,0 +1,12 @@
+const quotes = [
+  { quote: "A rose by any other name would smell as sweet.", writer: "William Shakespeare" },
+  { quote: "All that glitters is not gold.", writer: "William Shakespeare" },
+  { quote: "I'll be back.", writer: "Terminator" },
+  { quote: "Life is like a box of chocolates. You never know what you’re gonna get.", writer: "Forrest Gump" },
+  { quote: "Not all those who wander are lost.", writer: "J. R. R. Tolkien" },
+  { quote: "That’s one small step for a man, a giant leap for mankind.", writer: "Neil Armstrong" },
+  { quote: "What doesn't kill us makes us stronger.", writer: "Friedrich Nietzsche" },
+  { quote: "United we stand, divided we fall.", writer: "Aesop" }
+];
+
+export default quotes;
